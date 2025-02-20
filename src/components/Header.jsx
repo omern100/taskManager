fe0c34bdb2d970569a452db18675cf33a85263f2
omern100 <omern100@gmail.com>
2025-02-20 20:27:13 +0200

    changeNames

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <div className="header text-center">
-      <h2>Omer Nagar</h2>
+      <h2>Omer Nagar & Asaf Elgali</h2>
     </div>
   );
 };
